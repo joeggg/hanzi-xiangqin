34b173f3b8b4152edd1c9201833a15ae99a36747
joeggg <47352958+joeggg@users.noreply.github.com>
2025-03-02 00:42:28 +0000

    feat: load character definitions in from cedict

--- hanzi_xiangqin/data_types.py
+++ hanzi_xiangqin/data_types.py
@@ -1,6 +1,14 @@
+import re
+from collections import defaultdict
 from dataclasses import dataclass
 
 from .config import get_config
+
+
+@dataclass
+class Definition:
+    pinyin: str
+    text: str
 
 
 @dataclass
@@ -8,10 +16,26 @@ class Hanzi:
     simplified: str
     traditional: str
     rank: int
+    definitions: list[Definition]
+
+
+def load_dictionary() -> dict[str, list[Definition]]:
+    dictionary = defaultdict(list)
+    config = get_config()
+    pattern = re.compile(r"^(.) . \[(.*)\] (.*)$")
+    with open(f"{config.data_dir}/cedict_ts.u8") as f:
+        for line in f:
+            if not (match := pattern.match(line)):
+                continue
+
+            simplified, pinyin, definition = match.groups()
+            dictionary[simplified].append(Definition(pinyin, definition[1:-1]))
+    return dictionary
 
 
 def load_character_list() -> list[Hanzi]:
     config = get_config()
+    dictionary = load_dictionary()
 
     with open(f"{config.data_dir}/hanzi.txt") as f:
         simplified_lines = f.readlines()
@@ -25,6 +49,16 @@ def load_character_list() -> list[Hanzi]:
     chars = []
     for i, (s_line, t_line) in enumerate(zip(simplified_lines, traditional_lines)):
         if i % 2 == 0:
-            chars.append(Hanzi(s_line.strip(), t_line.strip(), i + 1))
+            simplified = s_line.strip()
+            traditional = t_line.strip()
+
+            chars.append(
+                Hanzi(
+                    simplified,
+                    traditional,
+                    i + 1,
+                    dictionary.get(simplified, dictionary.get(traditional, [])),
+                )
+            )
 
     return chars
