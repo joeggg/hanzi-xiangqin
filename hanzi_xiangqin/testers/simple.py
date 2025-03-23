38bb29d2c1d2f357ea797ffc259a8774c3c4975d
joeggg <47352958+joeggg@users.noreply.github.com>
2025-03-23 23:52:33 +0000

    feat: improved simple tester character generation algo

--- hanzi_xiangqin/testers/simple.py
+++ hanzi_xiangqin/testers/simple.py
@@ -13,20 +13,54 @@ class GuessResults:
     correct: int = 0
     incorrect: int = 0
 
+    def __add__(self, other: "GuessResults") -> "GuessResults":
+        return GuessResults(
+            correct=self.correct + other.correct, incorrect=self.incorrect + other.incorrect
+        )
+
+    def ratio(self) -> float:
+        return self.correct / (self.correct + self.incorrect)
+
 
 class SimpleTester(Tester):
-    def __init__(self, chars: list[Hanzi], bin_size: int = 500) -> None:
+    """
+    Splits character into bins based on how common they are, and begins with the first bin
+
+    Will serve `chars_per_level` characters from the current bin before advancing to the next bin
+
+    Once the first bin is reached where none are correct, it will reduce the bin by 1, then will
+    enter a state where it will either increase the bin or decrease the bin after each set of
+    characters based on if the ratio of correct answers is above or below 50%.
+
+    Will repeat this process until it has revisited the same bin `max_level_repeats` times
+    """
+
+    def __init__(
+        self,
+        chars: list[Hanzi],
+        bin_size: int = 500,
+        chars_per_level: int = 4,
+        max_level_repeats: int = 2,
+    ) -> None:
         super().__init__(chars)
-        self.bin_size = bin_size
-        self.answers: dict[int, GuessResults] = defaultdict(GuessResults)
         self.name = "simple"
 
-    def characters(self) -> Generator[Hanzi, bool, None]:
-        bins = list(itertools.batched(self.chars, 500))
+        self.bin_size = bin_size
+        self.chars_per_level = chars_per_level
+        self.max_level_repeats = max_level_repeats
 
-        current_bin = 0
+        self.seen_chars: set[Hanzi] = set()
+        self.answers: dict[int, GuessResults] = defaultdict(GuessResults)
+        self.bins = list(itertools.batched(self.chars, self.bin_size))
+        self.second_stage = False
+
+    def characters(self) -> Generator[Hanzi, bool, None]:
+        """Serve characters and receive answers using a generator until the test is over"""
+        current_bin, char_no = 0, 0
+        current_results = GuessResults()
         times_visited: dict[int, int] = defaultdict(int)
-        char = random.choice(bins[current_bin])
+
+        char = self._get_character(current_bin)
 
         while True:
             answer = yield char
@@ -34,21 +68,57 @@ class SimpleTester(Tester):
             if answer is None:
                 continue
 
-            times_visited[current_bin] += 1
+            char_no += 1
 
             if answer:
-                self.answers[current_bin].correct += 1
-                if current_bin != len(bins) - 1:
-                    current_bin += 1
+                current_results.correct += 1
             else:
-                self.answers[current_bin].incorrect += 1
-                if current_bin != 0:
-                    current_bin -= 1
+                current_results.incorrect += 1
 
-            if times_visited[current_bin] == 4:
-                break
+            # Change level once number of characters reached
+            if char_no % self.chars_per_level == 0:
+                self.answers[current_bin] += current_results
 
-            char = random.choice(bins[current_bin])
+                if self.second_stage:
+                    # In 2nd stage inc/decr bin based on ratio, break if bin visited too many times
+                    times_visited[current_bin] += 1
+                    if times_visited[current_bin] == self.max_level_repeats:
+                        break
+
+                    if current_results.ratio() >= 0.5:
+                        current_bin = self._incr_bin(current_bin)
+                    else:
+                        current_bin = self._decr_bin(current_bin)
+
+                else:
+                    # In 1st stage, inc bin each time until none correct or reached last bin
+                    if current_results.correct == 0 or current_bin == len(self.bins) - 1:
+                        self.second_stage = True
+                    else:
+                        current_bin = self._incr_bin(current_bin)
+
+                current_results = GuessResults()
+
+            char = self._get_character(current_bin)
+
+    def _incr_bin(self, current_bin: int) -> int:
+        """Increment bin if below the maximum"""
+        if current_bin == len(self.bins) - 1:
+            return current_bin
+        return current_bin + 1
+
+    def _decr_bin(self, current_bin: int) -> int:
+        """Decrease bin if above the minimum"""
+        if current_bin == 0:
+            return current_bin
+        return current_bin - 1
+
+    def _get_character(self, bin: int) -> Hanzi:
+        """Get a unique random character from the given bin"""
+        while (char := random.choice(self.bins[bin])) in self.seen_chars:
+            ...
+        self.seen_chars.add(char)
+        return char
 
     def estimate_count(self) -> int:
         correct_ratios = {
